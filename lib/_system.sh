a7e7eb33cb4b8176d719c1be6be2222bc010ed8d
LucasCarlim <109760460+LcsCarlim@users.noreply.github.com>
2024-05-07 12:22:20 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -1,51 +1,41 @@
 #!/bin/bash
 # 
-# system management
+# Gerenciamento do sistema
 
 #######################################
-# creates user
-# Arguments:
-#   None
+# Cria usuário
+# Argumentos:
+#   Nenhum
 #######################################
 system_create_user() {
   print_banner
-  printf "${WHITE} 💻 Agora, vamos criar o usuário para a instancia...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Agora, vamos criar o usuário para a instância...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
 
-  sudo su - root <<EOF
-  useradd -m -p $(openssl passwd -crypt ${mysql_root_password}) -s /bin/bash -G sudo deploy
-  usermod -aG sudo deploy
-EOF
-
-  sleep 2
+  sudo useradd -m -p $(openssl passwd -crypt ${mysql_root_password}) -s /bin/bash -G wheel deploy
 }
 
 #######################################
-# clones repostories using git
-# Arguments:
-#   None
+# Clona repositórios usando git
+# Argumentos:
+#   Nenhum
 #######################################
 system_git_clone() {
   print_banner
   printf "${WHITE} 💻 Fazendo download do código Whaticket...${GRAY_LIGHT}"
   printf "\n\n"
 
-
   sleep 2
 
-  sudo su - deploy <<EOF
-  git clone ${link_git} /home/deploy/${instancia_add}/
-EOF
-
-  sleep 2
+  sudo -u deploy git clone ${link_git} /home/deploy/${instancia_add}/
 }
 
 #######################################
-# updates system
-# Arguments:
-#   None
+# Atualiza sistema
+# Argumentos:
+#   Nenhum
 #######################################
 system_update() {
   print_banner
@@ -54,20 +44,19 @@ system_update() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  apt -y update
-  sudo apt-get install -y libxshmfence-dev libgbm-dev wget unzip fontconfig locales gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
-EOF
-
-  sleep 2
+  sudo yum -y update
+  sudo yum -y install epel-release
+  sudo yum -y install libxshmfence-devel libgbm-devel wget unzip fontconfig gconf-service \
+    libasound2 atk cairo cups dbus expat fontconfig glib2 gtk3 nspr pango \
+    libstdc++ libX11 libX11-xcb libxcb libXcomposite libXcursor libXdamage \
+    libXext libXfixes libXi libXrandr libXrender libXtst ca-certificates \
+    liberation-fonts libappindicator nss lsb-release xdg-utils
 }
 
-
-
 #######################################
-# delete system
-# Arguments:
-#   None
+# Deleta sistema
+# Argumentos:
+#   Nenhum
 #######################################
 deletar_tudo() {
   print_banner
@@ -76,44 +65,28 @@ deletar_tudo() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  docker container rm redis-${empresa_delete} --force
-  cd && rm -rf /etc/nginx/sites-enabled/${empresa_delete}-frontend
-  cd && rm -rf /etc/nginx/sites-enabled/${empresa_delete}-backend  
-  cd && rm -rf /etc/nginx/sites-available/${empresa_delete}-frontend
-  cd && rm -rf /etc/nginx/sites-available/${empresa_delete}-backend
+  sudo docker container rm redis-${empresa_delete} --force
+  sudo rm -rf /etc/nginx/sites-enabled/${empresa_delete}-frontend
+  sudo rm -rf /etc/nginx/sites-enabled/${empresa_delete}-backend  
+  sudo rm -rf /etc/nginx/sites-available/${empresa_delete}-frontend
+  sudo rm -rf /etc/nginx/sites-available/${empresa_delete}-backend
   
-  sleep 2
+  sudo -u postgres dropuser ${empresa_delete}
+  sudo -u postgres dropdb ${empresa_delete}
 
-  sudo su - postgres
-  dropuser ${empresa_delete}
-  dropdb ${empresa_delete}
-  exit
-EOF
-
-sleep 2
-
-sudo su - deploy <<EOF
- rm -rf /home/deploy/${empresa_delete}
- pm2 delete ${empresa_delete}-frontend ${empresa_delete}-backend
- pm2 save
-EOF
-
-  sleep 2
+  sudo -u deploy rm -rf /home/deploy/${empresa_delete}
+  sudo pm2 delete ${empresa_delete}-frontend ${empresa_delete}-backend
+  sudo pm2 save
 
   print_banner
   printf "${WHITE} 💻 Remoção da Instancia/Empresa ${empresa_delete} realizado com sucesso ...${GRAY_LIGHT}"
   printf "\n\n"
-
-
-  sleep 2
-
 }
 
 #######################################
-# bloquear system
-# Arguments:
-#   None
+# Bloqueia sistema
+# Argumentos:
+#   Nenhum
 #######################################
 configurar_bloqueio() {
   print_banner
@@ -122,25 +95,18 @@ configurar_bloqueio() {
 
   sleep 2
 
-sudo su - deploy <<EOF
- pm2 stop ${empresa_bloquear}-backend
- pm2 save
-EOF
-
-  sleep 2
+  sudo -u deploy pm2 stop ${empresa_bloquear}-backend
+  sudo -u deploy pm2 save
 
   print_banner
   printf "${WHITE} 💻 Bloqueio da Instancia/Empresa ${empresa_bloquear} realizado com sucesso ...${GRAY_LIGHT}"
   printf "\n\n"
-
-  sleep 2
 }
 
-
 #######################################
-# desbloquear system
-# Arguments:
-#   None
+# Desbloqueia sistema
+# Argumentos:
+#   Nenhum
 #######################################
 configurar_desbloqueio() {
   print_banner
@@ -149,55 +115,37 @@ configurar_desbloqueio() {
 
   sleep 2
 
-sudo su - deploy <<EOF
- pm2 start ${empresa_bloquear}-backend
- pm2 save
-EOF
-
-  sleep 2
+  sudo -u deploy pm2 start ${empresa_bloquear}-backend
+  sudo -u deploy pm2 save
 
   print_banner
   printf "${WHITE} 💻 Desbloqueio da Instancia/Empresa ${empresa_desbloquear} realizado com sucesso ...${GRAY_LIGHT}"
   printf "\n\n"
-
-  sleep 2
 }
 
 #######################################
-# alter dominio system
-# Arguments:
-#   None
+# Altera domínio do sistema
+# Argumentos:
+#   Nenhum
 #######################################
 configurar_dominio() {
   print_banner
-  printf "${WHITE} 💻 Vamos Alterar os Dominios do Whaticket...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos Alterar os Domínios do Whaticket...${GRAY_LIGHT}"
   printf "\n\n"
 
-sleep 2
+  sleep 2
 
-  sudo su - root <<EOF
-  cd && rm -rf /etc/nginx/sites-enabled/${empresa_dominio}-frontend
-  cd && rm -rf /etc/nginx/sites-enabled/${empresa_dominio}-backend  
-  cd && rm -rf /etc/nginx/sites-available/${empresa_dominio}-frontend
-  cd && rm -rf /etc/nginx/sites-available/${empresa_dominio}-backend
-EOF
+  sudo rm -rf /etc/nginx/sites-enabled/${empresa_dominio}-frontend
+  sudo rm -rf /etc/nginx/sites-enabled/${empresa_dominio}-backend  
+  sudo rm -rf /etc/nginx/sites-available/${empresa_dominio}-frontend
+  sudo rm -rf /etc/nginx/sites-available/${empresa_dominio}-backend
+  
+  sudo -u deploy sed -i "1c\REACT_APP_BACKEND_URL=https://${alter_backend_url}" /home/deploy/${empresa_dominio}/frontend/.env
+  sudo -u deploy sed -i "2c\BACKEND_URL=https://${alter_backend_url}" /home/deploy/${empresa_dominio}/backend/.env
+  sudo -u deploy sed -i "3c\FRONTEND_URL=https://${alter_frontend_url}" /home/deploy/${empresa_dominio}/backend/.env 
 
-sleep 2
-
-  sudo su - deploy <<EOF
-  cd && cd /home/deploy/${empresa_dominio}/frontend
-  sed -i "1c\REACT_APP_BACKEND_URL=https://${alter_backend_url}" .env
-  cd && cd /home/deploy/${empresa_dominio}/backend
-  sed -i "2c\BACKEND_URL=https://${alter_backend_url}" .env
-  sed -i "3c\FRONTEND_URL=https://${alter_frontend_url}" .env 
-EOF
-
-sleep 2
-   
-   backend_hostname=$(echo "${alter_backend_url/https:\/\/}")
-
- sudo su - root <<EOF
-  cat > /etc/nginx/sites-available/${empresa_dominio}-backend << 'END'
+  backend_hostname=$(echo "${alter_backend_url/https:\/\/}")
+  sudo su - root -c "cat > /etc/nginx/sites-available/${empresa_dominio}-backend << 'END'
 server {
   server_name $backend_hostname;
   location / {
@@ -212,16 +160,11 @@ server {
     proxy_cache_bypass \$http_upgrade;
   }
 }
-END
-ln -s /etc/nginx/sites-available/${empresa_dominio}-backend /etc/nginx/sites-enabled
-EOF
+END"
+  ln -s /etc/nginx/sites-available/${empresa_dominio}-backend /etc/nginx/sites-enabled
 
-sleep 2
-
-frontend_hostname=$(echo "${alter_frontend_url/https:\/\/}")
-
-sudo su - root << EOF
-cat > /etc/nginx/sites-available/${empresa_dominio}-frontend << 'END'
+  frontend_hostname=$(echo "${alter_frontend_url/https:\/\/}")
+  sudo su - root -c "cat > /etc/nginx/sites-available/${empresa_dominio}-frontend << 'END'
 server {
   server_name $frontend_hostname;
   location / {
@@ -236,157 +179,89 @@ server {
     proxy_cache_bypass \$http_upgrade;
   }
 }
-END
-ln -s /etc/nginx/sites-available/${empresa_dominio}-frontend /etc/nginx/sites-enabled
-EOF
+END"
+  ln -s /etc/nginx/sites-available/${empresa_dominio}-frontend /etc/nginx/sites-enabled
 
- sleep 2
-
- sudo su - root <<EOF
   service nginx restart
-EOF
-
-  sleep 2
 
   backend_domain=$(echo "${backend_url/https:\/\/}")
   frontend_domain=$(echo "${frontend_url/https:\/\/}")
 
-  sudo su - root <<EOF
   certbot -m $deploy_email \
           --nginx \
           --agree-tos \
           --non-interactive \
           --domains $backend_domain,$frontend_domain
-EOF
-
-  sleep 2
 
   print_banner
-  printf "${WHITE} 💻 Alteração de dominio da Instancia/Empresa ${empresa_dominio} realizado com sucesso ...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Alteração de domínio da Instancia/Empresa ${empresa_dominio} realizado com sucesso ...${GRAY_LIGHT}"
   printf "\n\n"
-
-  sleep 2
 }
 
 #######################################
-# installs node
-# Arguments:
-#   None
+# Instala node
+# Argumentos:
+#   Nenhum
 #######################################
 system_node_install() {
   print_banner
-  printf "${WHITE} 💻 Instalando node.js...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Instalando nodejs...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
 
-  sudo su - root <<EOF
-  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-  apt-get install -y nodejs
-  sleep 2
+  curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash -
+  sudo yum -y install nodejs
   npm install -g npm@latest
-  sleep 2
-  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-  sudo apt-get update -y && sudo apt-get -y install postgresql
-  sleep 2
-  sudo timedatectl set-timezone America/Sao_Paulo
-  
-EOF
+  sudo sh -c 'echo -e "[pgdg${lsb_release -rs}]\\nname=PostgreSQL $releasever - $basearch\\nbaseurl=https://download.postgresql.org/pub/repos/yum/reporpms/EL-\$releasever-\$basearch\\nenabled=1\\ngpgcheck=0" > /etc/yum.repos.d/pgdg.repo'
+  sudo yum -y install postgresql-server
+  sudo systemctl enable postgresql
+  sudo systemctl start postgresql
 
-  sleep 2
+  timedatectl set-timezone America/Sao_Paulo
 }
+
 #######################################
-# installs docker
-# Arguments:
-#   None
+# Instala docker
+# Argumentos:
+#   Nenhum
 #######################################
 system_docker_install() {
   print_banner
-  printf "${WHITE} 💻 Instalando redis...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Instalando docker...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
 
-  sudo su - root <<EOF
-    apt install redis-server -y
-    systemctl enable redis-server.service
-EOF
-
-  sleep 2
+  sudo yum -y install yum-utils
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  sudo yum -y install docker-ce docker-ce-cli containerd.io
+  sudo systemctl start docker
+  sudo systemctl enable docker
 }
 
 #######################################
-# Ask for file location containing
-# multiple URL for streaming.
-# Globals:
-#   WHITE
-#   GRAY_LIGHT
-#   BATCH_DIR
-#   PROJECT_ROOT
-# Arguments:
-#   None
+# Instala dependências do Puppeteer
+# Argumentos:
+#   Nenhum
 #######################################
 system_puppeteer_dependencies() {
   print_banner
-  printf "${WHITE} 💻 Instalando puppeteer dependencies...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Instalando dependências do puppeteer...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
 
-  sudo su - root <<EOF
-  apt-get install -y libxshmfence-dev \
-                      libgbm-dev \
-                      wget \
-                      unzip \
-                      fontconfig \
-                      locales \
-                      gconf-service \
-                      libasound2 \
-                      libatk1.0-0 \
-                      libc6 \
-                      libcairo2 \
-                      libcups2 \
-                      libdbus-1-3 \
-                      libexpat1 \
-                      libfontconfig1 \
-                      libgcc1 \
-                      libgconf-2-4 \
-                      libgdk-pixbuf2.0-0 \
-                      libglib2.0-0 \
-                      libgtk-3-0 \
-                      libnspr4 \
-                      libpango-1.0-0 \
-                      libpangocairo-1.0-0 \
-                      libstdc++6 \
-                      libx11-6 \
-                      libx11-xcb1 \
-                      libxcb1 \
-                      libxcomposite1 \
-                      libxcursor1 \
-                      libxdamage1 \
-                      libxext6 \
-                      libxfixes3 \
-                      libxi6 \
-                      libxrandr2 \
-                      libxrender1 \
-                      libxss1 \
-                      libxtst6 \
-                      ca-certificates \
-                      fonts-liberation \
-                      libappindicator1 \
-                      libnss3 \
-                      lsb-release \
-                      xdg-utils
-EOF
-
-  sleep 2
+  sudo yum -y install libXcomposite libXcursor libXi libXtst libXrandr \
+    alsa-lib atk gtk3 ipa-gothic-fonts xorg-x11-fonts-100dpi \
+    xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic \
+    xorg-x11-fonts-Type1 xorg-x11-fonts-misc
 }
 
 #######################################
-# installs pm2
-# Arguments:
-#   None
+# Instala pm2
+# Argumentos:
+#   Nenhum
 #######################################
 system_pm2_install() {
   print_banner
@@ -395,18 +270,13 @@ system_pm2_install() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  npm install -g pm2
-
-EOF
-
-  sleep 2
+  sudo npm install -g pm2
 }
 
 #######################################
-# installs snapd
-# Arguments:
-#   None
+# Instala snapd
+# Argumentos:
+#   Nenhum
 #######################################
 system_snapd_install() {
   print_banner
@@ -415,19 +285,17 @@ system_snapd_install() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  apt install -y snapd
-  snap install core
-  snap refresh core
-EOF
-
-  sleep 2
+  sudo yum -y install epel-release
+  sudo yum -y install snapd
+  sudo systemctl enable --now snapd.socket
+  sudo ln -s /var/lib/snapd/snap /snap
+  sudo systemctl start snapd
 }
 
 #######################################
-# installs certbot
-# Arguments:
-#   None
+# Instala certbot
+# Argumentos:
+#   Nenhum
 #######################################
 system_certbot_install() {
   print_banner
@@ -436,19 +304,15 @@ system_certbot_install() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  apt-get remove certbot
-  snap install --classic certbot
-  ln -s /snap/bin/certbot /usr/bin/certbot
-EOF
-
-  sleep 2
+  sudo yum -y remove certbot
+  sudo snap install --classic certbot
+  sudo ln -s /snap/bin/certbot /usr/bin/certbot
 }
 
 #######################################
-# installs nginx
-# Arguments:
-#   None
+# Instala nginx
+# Argumentos:
+#   Nenhum
 #######################################
 system_nginx_install() {
   print_banner
@@ -457,60 +321,46 @@ system_nginx_install() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  apt install -y nginx
-  rm /etc/nginx/sites-enabled/default
-EOF
-
-  sleep 2
+  sudo yum -y install nginx
+  sudo rm /etc/nginx/conf.d/default.conf
 }
 
 #######################################
-# restarts nginx
-# Arguments:
-#   None
+# Reinicia nginx
+# Argumentos:
+#   Nenhum
 #######################################
 system_nginx_restart() {
   print_banner
-  printf "${WHITE} 💻 reiniciando nginx...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Reiniciando nginx...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
 
-  sudo su - root <<EOF
-  service nginx restart
-EOF
-
-  sleep 2
+  sudo systemctl restart nginx
 }
 
 #######################################
-# setup for nginx.conf
-# Arguments:
-#   None
+# Configura nginx.conf
+# Argumentos:
+#   Nenhum
 #######################################
 system_nginx_conf() {
   print_banner
-  printf "${WHITE} 💻 configurando nginx...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Configurando nginx...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
 
-sudo su - root << EOF
-
-cat > /etc/nginx/conf.d/deploy.conf << 'END'
+  sudo su - root -c "cat > /etc/nginx/conf.d/deploy.conf << 'END'
 client_max_body_size 100M;
-END
-
-EOF
-
-  sleep 2
+END"
 }
 
 #######################################
-# installs nginx
-# Arguments:
-#   None
+# Configuração para nginx.conf
+# Argumentos:
+#   Nenhum
 #######################################
 system_certbot_setup() {
   print_banner
@@ -522,14 +372,11 @@ system_certbot_setup() {
   backend_domain=$(echo "${backend_url/https:\/\/}")
   frontend_domain=$(echo "${frontend_url/https:\/\/}")
 
-  sudo su - root <<EOF
-  certbot -m $deploy_email \
+  sudo certbot -m $deploy_email \
           --nginx \
           --agree-tos \
           --non-interactive \
           --domains $backend_domain,$frontend_domain
-
-EOF
-
-  sleep 2
 }
+
+# Agora você pode chamar as funções conforme necessário.
